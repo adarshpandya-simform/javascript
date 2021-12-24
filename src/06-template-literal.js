6615ac562b5bd17a27736f813ccbf23c0bae32c5
adarshpandya-simform <adarsh.p@simformsolutions.com>
2021-12-24 14:35:58 +0530

    TE5-T145 template literals

---
+++ src/06-template-literal.js
@@ -0,0 +1,14 @@
+// !to run this file use command `node ./src/filename.js`
+
+// template literals are written covered with backticks ``
+// we can embed any kind of variable inside a template literal string
+// variables should be inside ${variable_name}
+
+var name = "adarsh";
+var surname = "pandya";
+
+// normal way
+console.log(name + " " + surname);
+
+// template literal
+console.log(`${name} ${surname}`);
